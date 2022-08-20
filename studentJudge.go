b2684f8774c6247386ecd471b2472faf6fdea97f
小島和也 <kazuya@kojimakazuyanoMacBook-ea.local>
2022-08-21 00:54:26 +0900

    add studentJudge testprogram

--- studentJudge.go
+++ studentJudge.go
@@ -6,10 +6,6 @@ type Student struct {
 	name string
 }
 
-func judge() {
-
-}
-
 func (s Student) calAvg(data []float64) (avgResult float64) {
 	sum := 0.0
 
@@ -20,7 +16,22 @@ func (s Student) calAvg(data []float64) (avgResult float64) {
 	return
 }
 
+func (s Student) judge(avg float64) (judgeResult string) {
+	if avg >= 60 {
+		judgeResult = "合格"
+	} else if avg <= 30 {
+		judgeResult = "赤点"
+	} else {
+		judgeResult = "不合格"
+	}
+	return
+}
+
 func main() {
 	a001 := Student{"Kojima"}
-	fmt.Println(a001.calAvg([]float64{70, 80, 90}))
+	data := []float64{90, 90, 90, 30, 20, 40}
+	var avg float64 = a001.calAvg(data)
+	result := a001.judge(avg)
+	fmt.Println(a001.name, "の平均点は、", avg, "点です")
+	fmt.Println(a001.name + "は、" + result)
 }
