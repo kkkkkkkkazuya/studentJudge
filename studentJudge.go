97221e63b8289ae1e9baced02eb5932c69796eaf
小島和也 <kazuya@kojimakazuyanoMacBook-ea.local>
2022-08-20 22:18:21 +0900

    add calAvg and view arrangement

--- studentJudge.go
+++ studentJudge.go
@@ -10,10 +10,17 @@ func judge() {
 
 }
 
-func calAvg() {
+func (s Student) calAvg(data []float64) (avgResult float64) {
+	sum := 0.0
 
+	for i := 0; i < len(data); i++ {
+		sum += data[i]
+	}
+	avgResult = sum / float64(len(data))
+	return
 }
 
 func main() {
-	fmt.Println("test")
+	a001 := Student{"Kojima"}
+	fmt.Println(a001.calAvg([]float64{70, 80, 90}))
 }
